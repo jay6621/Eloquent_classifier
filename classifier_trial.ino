6b07a8f518e9db60cf0a92c914fcc414ab1d6136
JAY RATHOD <jayr6621@gmail.com>
2020-07-04 00:11:43 +0530

    Update classifier_trial.ino

--- classifier_trial.ino
+++ classifier_trial.ino
@@ -20,8 +20,8 @@ Eloquent::ML::Port::SVM pr;
 //Eloquent::ML::Port::SVM
 uint16_t prev_frame[H][W] = { 0 };
 uint16_t current_frame[H][W] = { 0 };
-uint16_t rgb_frame[H][W][3] = { 0 };
-float features[H*W*3] = { 0 };
+uint16_t rgb_frame[H][W] = { 0 };
+float features[H*W] = { 0 };
 
 
 bool setup_camera(framesize_t);
@@ -53,7 +53,7 @@ void loop() {
 
     linearize_features();
     print_features();
-    //classify();
+    classify();
     delay(8000);
 }
 
@@ -83,7 +83,7 @@ bool setup_camera(framesize_t frameSize) {
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_RGB565;
+    config.pixel_format = PIXFORMAT_GRAYSCALE;
     config.frame_size = frameSize;
     config.jpeg_quality = 12;
     config.fb_count = 1;
@@ -96,53 +96,44 @@ bool setup_camera(framesize_t frameSize) {
     return ok;
 }
 
-
-/**
- * Convert RGB565 to RGB888
- */
-void convert_to_rgb(uint8_t *buf, size_t len) {
-    for (int y = 0; y < H; y++) {
-        for (int x = 0; x < W; x++) {
-            rgb_frame[y][x][0] = 0;
-            rgb_frame[y][x][1] = 0;
-            rgb_frame[y][x][2] = 0;
-        }
-    }
-
-    for (size_t i = 0; i < len; i += 2) {
-        const uint8_t high = buf[i];
-        const uint8_t low  = buf[i+1];
-        const uint16_t pixel = (high << 8) | low;
-
-        const uint8_t r = (pixel & 0b1111100000000000) >> 11;
-        const uint8_t g = (pixel & 0b0000011111100000) >> 6;
-        const uint8_t b = (pixel & 0b0000000000011111);
-
-        const size_t j = i / 2;
-        const uint16_t x = j % WIDTH;
-        const uint16_t y = floor(j / WIDTH);
-        const uint8_t block_x = floor(x / BLOCK_SIZE);
-        const uint8_t block_y = floor(y / BLOCK_SIZE);
-
-        // average pixels in block (accumulate)
-        rgb_frame[block_y][block_x][0] += r;
-        rgb_frame[block_y][block_x][1] += g;
-        rgb_frame[block_y][block_x][2] += b;
-    }
-}
-
 /**
  * Capture image and do down-sampling
  */
 bool capture_still() {
-    camera_fb_t *frame = esp_camera_fb_get();
+    camera_fb_t *frame_buffer = esp_camera_fb_get();
 
-    if (!frame)
+    if (!frame_buffer)
         return false;
-    else
-      return true;
-    convert_to_rgb(frame->buf, frame->len);
 
+    // set all 0s in current frame
+    for (int y = 0; y < H; y++)
+       { 
+        for (int x = 0; x < W; x++){
+          current_frame[y][x] = 0;
+       }
+       }    
+
+
+    // down-sample image in blocks
+    for (uint32_t i = 0; i < WIDTH * HEIGHT; i++) {
+        const uint16_t x = i % WIDTH;
+        const uint16_t y = floor(i / WIDTH);
+        const uint8_t block_x = floor(x / BLOCK_SIZE);
+        const uint8_t block_y = floor(y / BLOCK_SIZE);
+        const uint8_t pixel = frame_buffer->buf[i];
+        const uint16_t current = current_frame[block_y][block_x];
+
+        // average pixels in block (accumulate)
+        current_frame[block_y][block_x] += pixel;
+    }
+
+    // average pixels in block (rescale)
+    for (int y = 0; y < H; y++)
+        {
+          for (int x = 0; x < W; x++){
+            current_frame[y][x] /= BLOCK_SIZE * BLOCK_SIZE;
+        }
+      }
     
 }
 
@@ -155,9 +146,7 @@ void linearize_features() {
 
     for (int y = 0; y < H; y++) {
         for (int x = 0; x < W; x++) {
-            features[i++] = rgb_frame[y][x][0];
-            features[i++] = rgb_frame[y][x][1];
-            features[i++] = rgb_frame[y][x][2];
+            features[i++] = current_frame[y][x];
         }
     }
 }
@@ -167,7 +156,7 @@ void linearize_features() {
  *
  */
 void print_features() {
-    for (size_t i = 0; i < H*W*3; i++) {
+    for (size_t i = 0; i < H*W; i++) {
         Serial.print(features[i]);
         Serial.print('\t');
     }
@@ -177,7 +166,7 @@ void print_features() {
  * Run the inference
  */
 void classify() {
-    Serial.print("Object: ");
+    Serial.println("Object: ");
     Serial.println(classIdxToName(pr.predict(features)));
 }
 String classIdxToName(int clas){
